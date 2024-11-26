6d18c3cdb9e00d5518030b00aaaa6a9a07f33aa9
Carlos <sin correo>
2024-11-26 20:19:18 +0100

    Llevados los estilos de EpisodioComponent.vue a episodio-component.scss

--- src/components/EpisodioComponent.vue
+++ src/components/EpisodioComponent.vue
@@ -5,43 +5,10 @@
             <div class="episodio">{{ episodio.episode }}</div>
         </router-link>
     </div>
-    
-    <!-- <div class="contenedor">
-        <div class="nombre">{{ episodio.name }}</div>
-        <div class="episodio">{{ episodio.episode }}</div>
-    </div> -->
 </template>
 
 <style scoped>
-    .contenedor-episodio {
-        border-radius: 10px;
-        background-color: red;
-        padding: 2px;
-    }
-
-    a {
-        text-decoration: none;
-        color: white;
-    }
-    
-    .nombre,.episodio {
-        text-align: center;
-    }
-
-    .nombre {
-        font-size: 30px;
-    }
-    /* .contenedor {
-        border: 1px solid black;
-        border-radius: 10px;
-        width: 500px;
-        margin: 20px;
-        height: fit-content;
-    }
-    .nombre {
-        font-size: 60px;
-        text-align: center;
-    } */
+    @import '../assets/scss/episodio-component.scss';
 </style>
 
 <script setup>
